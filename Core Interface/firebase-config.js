b17970a99ac5443e3f6cdb806f9e257784ec3f2e
aqldrum <aqldrum@gmail.com>
2026-01-23 18:42:20 -0500

    Partitions updates, more samples

---
+++ Core Interface/firebase-config.js
@@ -0,0 +1,13 @@
+
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyDTiovjZrjaNdbhwR-SRTGpALVqi_XVYbU",
+  authDomain: "lrc-collections.firebaseapp.com",
+  projectId: "lrc-collections",
+  storageBucket: "lrc-collections.firebasestorage.app",
+  messagingSenderId: "382823794325",
+  appId: "1:382823794325:web:494e7706a4d9ac8b09bd9c",
+  measurementId: "G-F9V0T8PK89",
+    // NOTE: Firestore does not require databaseURL when using projectId + default instance.
+};
+
